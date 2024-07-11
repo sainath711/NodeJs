f11c31c5bedde135ce54a30a6c65e1d883486e43
sainath711 <sainathsaii007@gmail.com>
2024-07-11 10:13:43 +0530

    Static web Page using Express

---
+++ Static-Web/app.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import path from 'path'
+import chalk from 'chalk'
+
+
+//create express app
+let app = express()
+
+app.use(morgan('dev'))
+//load configuration env variable
+dotenv.config({path:"./config/config.env"})
+let port = process.env.PORT
+let hostname=process.env.HOST_NAME
+
+//create root req api
+//url: http://localhost:8080/
+app.get("/",(req,resp)=>{
+    resp.sendFile(path.join(process.cwd(),"web","index.html"),(err)=>{})
+})
+app.get("/index",(req,resp)=>{
+    resp.sendFile(path.join(process.cwd(),"web","index.html"),(err)=>{})
+})
+app.get("/about",(req,resp)=>{
+    resp.sendFile(path.join(process.cwd(),"web","about.html"),(err)=>{})
+})
+app.get("/services",(req,resp)=>{
+    resp.sendFile(path.join(process.cwd(),"web","services.html"),(err)=>{})
+})
+app.get("/contact",(req,resp)=>{
+    resp.sendFile(path.join(process.cwd(),"web","contact.html"),(err)=>{})
+})
+
+
+app.listen(port,hostname,(err)=>{
+    if(err) throw err 
+    console.log(chalk.bgWhite(),`server running on http://${hostname}:${port}/`)
+})
