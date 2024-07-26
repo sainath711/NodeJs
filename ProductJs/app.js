744e60df250128dfb766aaedd3d5e8d80b03ebd2
sainath711 <sainathsaii007@gmail.com>
2024-07-26 18:00:17 +0530

    node

---
+++ ProductJs/app.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import chalk from 'chalk'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import productRouter from './routes/ProductRouter.js'
+let app = express()
+
+app.use(cors())
+app.use(morgan('dev'))
+
+dotenv.config({path:'./config/dev.env'})
+let port = process.env.PORT 
+let host=process.env.HOST_NAME
+let db_url=process.env.MONGO_DB_LOCAL_URL
+app.get("/",(req,resp)=>{
+    resp.send("Server -Root Req")
+})
+app.use("/product",productRouter)
+mongoose.connect(db_url)
+.then(()=>{
+    console.log("Mongo Db Connection Successfull!")
+    app.listen(port,host,(err)=>{
+        console.log(chalk.green(`Server is Running http://${host}:${port}/`))
+        })
+    })
+.catch((err)=>{
+    process.exit(1);
+})
---
+++ ProductJs/routes/ProductRouter.js
@@ -0,0 +1,33 @@
+import express from "express";
+import Product from '../models/Product.js'
+let router= express.Router();
+
+/*
+    url:http://127.0.0.1:8080/product/
+*/
+router.get("/",(req,resp)=>{
+    resp.json({"msg":"Product Root Request"})
+});
+/*
+    USAGE: get all products
+    URL: http://127.0.0.1:8080/product/read
+    MEthod:GET
+    Fields:none
+    Access Type:Public
+*/
+ router.get("/read",async(req,resp)=>{
+    try{
+       let products  = await Product.find()
+       return resp.status(200).json(products)
+    }
+    catch(err){
+        return resp.status(401).json({"msg":err.message})
+    }
+ });
+/*
+router.get();
+router.post();
+router.put();
+router.delete(); */
+
+export default router;
