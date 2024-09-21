e52fcc798129b9857847c4892271ee2e0158b0f2
sainath711 <sainathsaii007@gmail.com>
2024-09-21 11:41:18 +0530

    Crudjson1

--- CRUDJSON1/routes/apiRouter.js
+++ CRUDJSON1/routes/apiRouter.js
@@ -1,9 +1,25 @@
 import express from 'express'
 import fs from 'fs'
+//import { get } from 'http';
 let router=express.Router();
 
-router.post("/create",(req,resp)=>{
-    let emp=req.body;
+router.post("/create",async(req,resp)=>{
+    
+    let employee=req.body;
+    let employees=await getEmployees()
+
+    let emp_obj=employees.find((emp)=>{
+       return emp.eid==emp.eid;
+    })
+    console.log(emp_obj)
+    if(emp_obj){
+        return resp.status(401).json({"msg":"Employe already exist"})
+    }
+    employees.push(employee)
+    savemployees(employees)
+        return resp.status(200).json({"msg":"New employee created"})
+    
+   
 });
 router.get("/read",async(req,resp)=>{
   let employees=await getEmployees()
@@ -19,5 +35,8 @@ router.delete("/del/:id",(req,resp)=>{
 let getEmployees=()=>{
     let emp_Data=fs.readFileSync('data.json','utf-8')
     return JSON.parse(emp_Data)
+}
+let savemployees = (employees)=>{
+    fs.writeFileSync('data.json',JSON.stringify(employees))
 }
 export default router;
